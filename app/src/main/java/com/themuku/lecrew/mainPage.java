82bd3ee4c6f14a10d16f3ae7897f4306d5d035c0
MusicJoker <43328204+MusicJoker@users.noreply.github.com>
2019-03-16 18:34:44 -0400

    .

--- app/src/main/java/com/themuku/lecrew/mainPage.java
+++ app/src/main/java/com/themuku/lecrew/mainPage.java
@@ -9,5 +9,7 @@ public class mainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        hm
     }
 }
