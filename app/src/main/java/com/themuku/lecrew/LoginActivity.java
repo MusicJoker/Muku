ca512896183034c4a60928a84aa632f4286d29b2
MusicJoker <43328204+MusicJoker@users.noreply.github.com>
2019-03-17 16:21:12 -0400

    .

---
+++ app/src/main/java/com/themuku/lecrew/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.themuku.lecrew;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//This is for the open page or you can see the loading page when you open the app.
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+
+
+    }
+}
