84499d6c54a480cb69802edb3e5da0bf0c6f03e4
MusicJoker <43328204+MusicJoker@users.noreply.github.com>
2019-03-24 22:26:14 -0400

    .

--- app/src/main/java/com/themuku/lecrew/SignupActivity.java
+++ app/src/main/java/com/themuku/lecrew/SignupActivity.java
@@ -1,15 +1,15 @@
 package com.themuku.lecrew;
 
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
 //This is for the home_page and to us this will be the main activity
-public class MainActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_page);
+        setContentView(R.layout.signup_page);
 
 
 
